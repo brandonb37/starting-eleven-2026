b2815494044caa654aa10f1ba614fdd5b0b2d892
brandonb37 <brandon@startingeleven.com>
2026-03-11 11:34:34 -0500

    Update contact link for directions 3

--- app/contact/page.js
+++ app/contact/page.js
@@ -24,7 +24,7 @@ export default function ContactPage() {
                 Kansas City, MO 64102
                 <br />
                 <a
-                  href="https://maps.app.goo.gl/9i745UvyVXKSAUvA9"
+                  href="https://maps.app.goo.gl/9hFnZMxFEbgBZVfz7"
                   target="_blank"
                 >
                   Get directions &rarr;
