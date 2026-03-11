6e8f354320e6a12fe94bbdccdb9d75d82557d2f4
brandonb37 <brandon@startingeleven.com>
2026-03-11 12:42:54 -0500

    Fix form tag by removing 'netlify' attribute

--- app/components/contactForm.js
+++ app/components/contactForm.js
@@ -59,7 +59,7 @@ export default function ContactForm() {
       <strong>Thank you for reaching out!</strong>
     </p>
   ) : (
-    <form netlify
+    <form
       action="/contact?success=true" 
       netlify
       name="contact"
