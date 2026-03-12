32e00658e7b5816c14ae1123d619e4f4e6feb817
brandonb37 <brandon@startingeleven.com>
2026-03-12 15:29:35 -0500

    Update header.js

--- app/components/header.js
+++ app/components/header.js
@@ -28,14 +28,14 @@ const HeaderWrap = styled.div`
 
 const LogoWrap = styled.a`
   display: flex;
-  height: 46px;
+  height: 60px;
   position: relative;
   width: 100%;
   max-width: 400px;
 
   img {
     width: 100%;
-    height: auto;
+    height: 100%;
   }
 `;
 
